fdde3af640ec63a2c5d7e66388b8005ef818526e
uh308759-sketch <uh308759@gmail.com>
2026-09-23 04:04:29 +0500

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -14,7 +14,7 @@ export async function onRequest(context) {
     <title>Welcome</title>
     <meta property="og:title" content="😍🎧🎬">
     <meta property="og:description" content="">
-    <meta property="og:image" content="https://i.pinimg.com/736x/67/69/c7/6769c7f868b87902001fc9170128c84a.jpg">
+    <meta property="og:image" content="https://share.google/htLVdRecF9WajtrEp">
     <meta property="og:url" content="https://www.google.com">
     <meta property="og:type" content="website">
 </head>
